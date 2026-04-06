1f681ff7f2f31d45304af1aa8760745e7d01b628
Aaron.Dai <alens.china@gmail.com>
2026-04-06 21:41:59 +0800

    build(android): strengthen debug apk config and add test resources

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
 }
 
 android {
@@ -33,9 +35,32 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            debugSymbolLevel = "FULL"
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    sourceSets {
+        getByName("debug").assets.srcDirs("src/debug/assets")
+        getByName("androidTest").assets.srcDirs("src/androidTest/assets")
     }
 
     buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+            isJniDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            signingConfig = signingConfigs.getByName("debug")
+            buildConfigField("Boolean", "ENABLE_VERBOSE_LOGGING", "true")
+        }
+
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
